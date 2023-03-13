22011352e3495e5534e329754db259b982039a6c
hkunkel2 <harrisonjameskunkel@gmail.com>
2023-03-13 09:11:38 -0400

    home page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,20 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+const home_video = document.getElementById("home_video");
+console.log(home_video)
+home_video.play();
+
+var text = "Welcome!";
+var delay = 100;
+
+function typeWriter(text, i) {
+  if (i < text.length) {
+    document.getElementById("typed-text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(function() { typeWriter(text, i) }, delay);
+  }
+}
+
+typeWriter(text, 0);
